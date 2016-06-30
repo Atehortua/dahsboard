97cca519be6daedf8e3ed522db232bc53c11622a
Atehortua <wyatehortua@gmail.com>
2016-06-30 16:39:57 -0500

    Poniendo botones de agregar factura y agregar ventas, imput de filtro con funcionalidad para borrar el texto escrito

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -5,5 +5,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
+
+    $scope.clearFiltro = function () {
+      $scope.filtroInventario = "";
+      console.log("limpiando Filtro")
+    }
   }
 ]);
