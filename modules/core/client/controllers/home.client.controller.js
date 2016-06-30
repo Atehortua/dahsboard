c8bdd42096a3db25550009a309191a524d6efb30
Atehortua <wyatehortua@gmail.com>
2016-06-30 01:20:10 -0500

    arreglando css de material icons

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$mdSidenav', '$mdBottomSheet', '$mdDialog','$mdToast',
-  function($scope, Authentication,  $mdSidenav, $mdBottomSheet, $mdDialog, $mdToast) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$mdSidenav', '$mdBottomSheet', '$mdDialog','$mdToast','$timeout',
+  function($scope, Authentication,  $mdSidenav, $mdBottomSheet, $mdDialog, $mdToast,$timeout) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
-    
+
   }
 ]);
