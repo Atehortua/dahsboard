80221cf2894d7cfff8da5dcfe6c2d3636bed7c3b
Atehortua <wyatehortua@gmail.com>
2016-07-06 18:06:38 -0500

    agregando vista del formulario y poniendo algunos input del formulario

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -1,14 +1,61 @@
 'use strict';
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$mdSidenav', '$mdBottomSheet', '$mdDialog','$mdToast','$timeout',
-  function($scope, Authentication,  $mdSidenav, $mdBottomSheet, $mdDialog, $mdToast,$timeout) {
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$mdSidenav', '$mdBottomSheet', '$mdDialog','$mdToast','$timeout','$mdMedia',
+  function($scope, Authentication,  $mdSidenav, $mdBottomSheet, $mdDialog, $mdToast, $timeout, $mdMedia) {
     // This provides Authentication context.
     $scope.authentication = Authentication;
 
+    $scope.status = '  ';
+    $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
 
     $scope.clearFiltro = function () {
       $scope.filtroInventario = "";
       console.log("limpiando Filtro")
     }
+    
+    $scope.showFormuFactura = function(ev) {
+      var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+      $mdDialog.show({
+        controller: DialogController,
+        templateUrl: 'formuFactura.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      })
+          .then(function(answer) {
+            $scope.status = 'You said the information was "' + answer + '".';
+          }, function() {
+            $scope.status = 'You cancelled the dialog.';
+          });
+      $scope.$watch(function() {
+        return $mdMedia('xs') || $mdMedia('sm');
+      }, function(wantsFullScreen) {
+        $scope.customFullscreen = (wantsFullScreen === true);
+      });
+    };
+    
   }
 ]);
+
+function DialogController($scope, $mdDialog) {
+  $scope.hide = function() {
+    $mdDialog.hide();
+  };
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+
+  $scope.formuProduct = false;
+  $scope.products = [];
+  $scope.addProduct = function () {
+    $scope.formuProduct = true;
+  };
+
+  $scope.cancelProduct = function () {
+    $scope.formuProduct = false;
+  };
+}
